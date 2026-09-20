33d7e3fb59c936a2e26781a45d67cccffc282f2b
Danish-ali <0000danishsh@gmail.com>
2026-09-20 13:13:36 +0500

    fixed env

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
-import { viteSingleFile } from "vite-plugin-singlefile";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -31,6 +31,7 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:4000',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
